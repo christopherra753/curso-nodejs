13762bdd5f8ab233c6368ce29ac1086157bc53da
christopher <christopher.romero.753@gmail.com>
2024-01-11 17:08:27 -0500

    Terminando la cuarta clase

---
+++ src/tercera-clase/schemas/movies.js
@@ -0,0 +1,26 @@
+const z = require('zod')
+
+const movieSchema = z.object({
+  title: z.string(),
+  year: z.number().int().min(1900).max(2024),
+  director: z.string(),
+  duration: z.number().int().positive(),
+  rate: z.number().min(0).max(10).default(5),
+  poster: z.string().url(),
+  genre: z.array(
+    z.enum(['Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy,"Crime'])
+  )
+})
+
+const validateMovie = (object) => {
+  return movieSchema.safeParse(object)
+}
+
+const validatePartialMovie = (objest) => {
+  return movieSchema.partial().safeParse(objest)
+}
+
+module.exports = {
+  validateMovie,
+  validatePartialMovie
+}
